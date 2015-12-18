f3ad2c86bd3722292422738a85189efc0367b68a
eglekaleckaite <arfineja@gmail.com>
2015-12-18 22:56:56 +0200

    Tables of the results.

--- Code/39simulationresults.R
+++ Code/39simulationresults.R
@@ -2,72 +2,117 @@ rm(list = ls())
 library(foreach)
 library(data.table)
 library(plyr)
+library(reshape2)
+library(xtable)
+
+############################################################################
+############################################################################
+## Compound statistics
+############################################################################
+############################################################################
+load("Output/mcmc_stats_all.RData")
+rres[Balansas == "UB" & P == "20", P := "P1"]
+rres[Balansas == "B" & P == "20", P := "P2"]
+rres[Balansas == "UB" & P == "35", P := "P3"]
+rres[Balansas == "B" & P == "35", P := "P4"]
+rres[Balansas == "UB" & P == "80", P := "P5"]
+rres[Balansas == "B" & P == "80", P := "P6"]
+rres[, Balansas := NULL]
+
+rres[V == "100_50_100", V := "V1"]
+rres[V == "800_400_800", V := "V2"]
+rres[V == "2000_1000_2000", V := "V3"]
+
+fres <- rres[Parametras %in% c("g00", "g01", "g10", "g11"), ]
+rrres <- rres[Parametras %in% c("sigma2", "tau00", "tau01", "tau11"), ]
+
+fcres <- fres[, list(CAMRBIAS = mean(abs(MRBIAS)), CRMSE = mean(MRMSE)),
+              by = c("Paklaidos", "Metodas", "P", "V")]
+rcres <- rrres[, list(CAMRBIAS = mean(abs(MRBIAS)), CRMSE = mean(MRMSE)),
+               by = c("Paklaidos", "Metodas", "P", "V")]
 
 
-load("Output/mcmcY1_simulMy.RData")
-load("Output/mcmcY2_simulMy.RData")
+fcres <- arrange(fcres, P, V, Paklaidos, Metodas)
+
+rcres <- arrange(rcres, P, V, Paklaidos, Metodas)
+
+ffcres <- cbind(fcres[Metodas == "REML", ], fcres[Metodas == "MINQUE(0)", ],
+                fcres[Metodas == "MINQUE(1)", ], fcres[Metodas == "MINQUE(th)", ])
+
+ffcres[, c(1, 2, 7:10, 13:16, 19:22) := NULL]
+
+rrcres <- cbind(rcres[Metodas == "REML", ], rcres[Metodas == "MINQUE(0)", ],
+                rcres[Metodas == "MINQUE(1)", ], rcres[Metodas == "MINQUE(th)", ])
+
+rrcres[, c(1, 2, 7:10, 13:16, 19:22) := NULL]
+
+print( xtable(ffcres, digits=3), include.rownames=FALSE )
+print( xtable(rrcres, digits=3), include.rownames=FALSE )
+
 ############################################################################
 ############################################################################
 ## Results statistics
 ############################################################################
 ############################################################################
-# unscale
-trth <- data.table(Parametras = c("g00", "g01", "g10", "g11", "sigma2"),
-                   Tikroji = c(450, 10, 30, 5, 2000))
-
-rres <- foreach(cc = c("g00", "g01", "g10", "g11", "sigma2"), .combine = rbind) %do% {
-  data.table(Parametras = cc, Paklaidos = "N", 
-             ldply(rresY1[[cc]],
-                   function(x) ldply(x, function(y) ldply(y, function(z) ldply(z, function(w){
-                     w[w<0] <- 0
-                     dd <- data.table(th = mean(w), MRBIAS = mean(w/trth[Parametras == cc, Tikroji]-1), MRMSE = mean((w/trth[Parametras == cc, Tikroji]-1)^2))
-                     return(dd)
-                   }, .id = "V"), .id = "P"), .id = "Balansas"), .id = "Metodas"))
-}
-
-
-rres2 <- foreach(cc = c("g00", "g01", "g10", "g11", "sigma2"), .combine = rbind) %do% {
-  data.table(Parametras = cc, Paklaidos = "X", 
-             ldply(rresY2[[cc]],
-                   function(x) ldply(x, function(y) ldply(y, function(z) ldply(z, function(w){
-                     w[w<0] <- 0
-                     dd <- data.table(th = mean(w), MRBIAS = mean(w/trth[Parametras == cc, Tikroji]-1), MRMSE = mean((w/trth[Parametras == cc, Tikroji]-1)^2))
-                     return(dd)
-                   }, .id = "V"), .id = "P"), .id = "Balansas"), .id = "Metodas"))
-}
-
-ttau <- data.table(V = c("100_50_100", "800_400_800", "2000_1000_2000"), 
-                   tau00 = c(100, 800, 2000),tau01 = c(50, 400, 1000),
-                   tau11 = c(100, 800, 2000))
-
-rres3 <- foreach(cc = c("tau00", "tau01", "tau11"), .combine = rbind) %do% {
-  data.table(Parametras = cc, Paklaidos = "X", 
-             ldply(rresY2[[cc]],
-                   function(x) ldply(x, function(y) ldply(y, function(z) {
-                     foreach(bb = names(z), .combine = rbind) %do% {
-                       w <- z[[bb]]
-                       w[w<0] <- 0
-                       dd <- data.table(V = bb, th = mean(w), MRBIAS = mean(w/unlist(ttau[V == bb, cc, with = F])-1), MRMSE = mean((w/unlist(ttau[V == bb, cc, with = F])-1)^2))
-                       return(dd)
-                     }}, .id = "P"), .id = "Balansas"), .id = "Metodas"))
-}
-
-rres4 <- foreach(cc = c("tau00", "tau01", "tau11"), .combine = rbind) %do% {
-  data.table(Parametras = cc, Paklaidos = "N", 
-             ldply(rresY1[[cc]],
-                   function(x) ldply(x, function(y) ldply(y, function(z) {
-                     foreach(bb = names(z), .combine = rbind) %do% {
-                       w <- z[[bb]]
-                       w[w<0] <- 0
-                       dd <- data.table(V = bb, th = mean(w), MRBIAS = mean(w/unlist(ttau[V == bb, cc, with = F])-1), MRMSE = mean((w/unlist(ttau[V == bb, cc, with = F])-1)^2))
-                       return(dd)
-                     }}, .id = "P"), .id = "Balansas"), .id = "Metodas"))
-}
-
-
-rres <- rbind(rres, rres2, rres3, rres4)
-
-save(rres, file = "Output/mcmc_stats_all.RData")
+# load("Output/mcmcY1_simulMy.RData")
+# load("Output/mcmcY2_simulMy.RData")
+# 
+# trth <- data.table(Parametras = c("g00", "g01", "g10", "g11", "sigma2"),
+#                    Tikroji = c(450, 10, 30, 5, 2000))
+# 
+# rres <- foreach(cc = c("g00", "g01", "g10", "g11", "sigma2"), .combine = rbind) %do% {
+#   data.table(Parametras = cc, Paklaidos = "N", 
+#              ldply(rresY1[[cc]],
+#                    function(x) ldply(x, function(y) ldply(y, function(z) ldply(z, function(w){
+#                      w[w<0] <- 0
+#                      dd <- data.table(th = mean(w), MRBIAS = mean(w/trth[Parametras == cc, Tikroji]-1), MRMSE = mean((w/trth[Parametras == cc, Tikroji]-1)^2))
+#                      return(dd)
+#                    }, .id = "V"), .id = "P"), .id = "Balansas"), .id = "Metodas"))
+# }
+# 
+# 
+# rres2 <- foreach(cc = c("g00", "g01", "g10", "g11", "sigma2"), .combine = rbind) %do% {
+#   data.table(Parametras = cc, Paklaidos = "X", 
+#              ldply(rresY2[[cc]],
+#                    function(x) ldply(x, function(y) ldply(y, function(z) ldply(z, function(w){
+#                      w[w<0] <- 0
+#                      dd <- data.table(th = mean(w), MRBIAS = mean(w/trth[Parametras == cc, Tikroji]-1), MRMSE = mean((w/trth[Parametras == cc, Tikroji]-1)^2))
+#                      return(dd)
+#                    }, .id = "V"), .id = "P"), .id = "Balansas"), .id = "Metodas"))
+# }
+# 
+# ttau <- data.table(V = c("100_50_100", "800_400_800", "2000_1000_2000"), 
+#                    tau00 = c(100, 800, 2000),tau01 = c(50, 400, 1000),
+#                    tau11 = c(100, 800, 2000))
+# 
+# rres3 <- foreach(cc = c("tau00", "tau01", "tau11"), .combine = rbind) %do% {
+#   data.table(Parametras = cc, Paklaidos = "X", 
+#              ldply(rresY2[[cc]],
+#                    function(x) ldply(x, function(y) ldply(y, function(z) {
+#                      foreach(bb = names(z), .combine = rbind) %do% {
+#                        w <- z[[bb]]
+#                        w[w<0] <- 0
+#                        dd <- data.table(V = bb, th = mean(w), MRBIAS = mean(w/unlist(ttau[V == bb, cc, with = F])-1), MRMSE = mean((w/unlist(ttau[V == bb, cc, with = F])-1)^2))
+#                        return(dd)
+#                      }}, .id = "P"), .id = "Balansas"), .id = "Metodas"))
+# }
+# 
+# rres4 <- foreach(cc = c("tau00", "tau01", "tau11"), .combine = rbind) %do% {
+#   data.table(Parametras = cc, Paklaidos = "N", 
+#              ldply(rresY1[[cc]],
+#                    function(x) ldply(x, function(y) ldply(y, function(z) {
+#                      foreach(bb = names(z), .combine = rbind) %do% {
+#                        w <- z[[bb]]
+#                        w[w<0] <- 0
+#                        dd <- data.table(V = bb, th = mean(w), MRBIAS = mean(w/unlist(ttau[V == bb, cc, with = F])-1), MRMSE = mean((w/unlist(ttau[V == bb, cc, with = F])-1)^2))
+#                        return(dd)
+#                      }}, .id = "P"), .id = "Balansas"), .id = "Metodas"))
+# }
+# 
+# 
+# rres <- rbind(rres, rres2, rres3, rres4)
+# 
+# save(rres, file = "Output/mcmc_stats_all.RData")
 
 
 
